20bdb824e1102e9622949f4b71f114d221c25eec
Leonardo Gatica <lgatica@protonmail.com>
2016-11-13 02:52:51 -0300

    chore(src): First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# node-build
+
+[![Image Layers](https://imagelayers.io/badge/lgatica/node-build:latest.svg)](https://imagelayers.io/?images=lgatica/node-build:latest 'Get your own badge on imagelayers.io')
+[![Docker Stars](https://img.shields.io/docker/stars/lgatica/node-build.svg)](https://hub.docker.com/r/lgatica/node-build/)
+[![Docker Pulls](https://img.shields.io/docker/pulls/lgatica/node-build.svg)](https://hub.docker.com/r/lgatica/node-build/)
+
+> Docker Image for node apps with alpine linux and native dependencies
---
+++ build.sh
@@ -0,0 +1,28 @@
+docker build -t node-build:7.1.0 7.1.0
+docker tag node-build:7.1.0 lgatica/node-build
+docker tag node-build:7.1.0 lgatica/node-build:7.1.0
+docker tag node-build:7.1.0 lgatica/node-build:7.1
+docker tag node-build:7.1.0 lgatica/node-build:7
+docker tag node-build:7.1.0 lgatica/node-build:latest
+docker build -t node-build:6.9.1 6.9.1
+docker tag node-build:6.9.1 lgatica/node-build:6.9.1
+docker tag node-build:6.9.1 lgatica/node-build:6.9
+docker tag node-build:6.9.1 lgatica/node-build:6
+docker build -t node-build:4.6.2 4.6.2
+docker tag node-build:4.6.2 lgatica/node-build:4.6.2
+docker tag node-build:4.6.2 lgatica/node-build:4.6
+docker tag node-build:4.6.2 lgatica/node-build:4
+docker build -t node-build:7.1.0-onbuild 7.1.0/onbuild
+docker tag node-build:7.1.0-onbuild lgatica/node-build:7.1.0-onbuild
+docker tag node-build:7.1.0-onbuild lgatica/node-build:7.1-onbuild
+docker tag node-build:7.1.0-onbuild lgatica/node-build:7-onbuild
+docker tag node-build:7.1.0-onbuild lgatica/node-build:onbuild
+docker build -t node-build:6.9.1-onbuild 6.9.1/onbuild
+docker tag node-build:6.9.1-onbuild lgatica/node-build:6.9.1-onbuild
+docker tag node-build:6.9.1-onbuild lgatica/node-build:6.9-onbuild
+docker tag node-build:6.9.1-onbuild lgatica/node-build:6-onbuild
+docker build -t node-build:4.6.2-onbuild 4.6.2/onbuild
+docker tag node-build:4.6.2-onbuild lgatica/node-build:4.6.2-onbuild
+docker tag node-build:4.6.2-onbuild lgatica/node-build:4.6-onbuild
+docker tag node-build:4.6.2-onbuild lgatica/node-build:4-onbuild
+docker push lgatica/node-build
