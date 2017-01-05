a791239be5eac5a51f2ecaac740c7ba0b85c5136
Leonardo Gatica <lgatica@protonmail.com>
2017-01-05 15:38:07 +0000

    feat(node): Update node from 7.3.0 to 7.4.0

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-docker build -t node-build:7.3.0 7.3.0
-docker tag node-build:7.3.0 lgatica/node-build
-docker tag node-build:7.3.0 lgatica/node-build:7.3.0
-docker tag node-build:7.3.0 lgatica/node-build:7.3
-docker tag node-build:7.3.0 lgatica/node-build:7
-docker tag node-build:7.3.0 lgatica/node-build:latest
+docker build -t node-build:7.4.0 7.4.0
+docker tag node-build:7.4.0 lgatica/node-build
+docker tag node-build:7.4.0 lgatica/node-build:7.4.0
+docker tag node-build:7.4.0 lgatica/node-build:7.4
+docker tag node-build:7.4.0 lgatica/node-build:7
+docker tag node-build:7.4.0 lgatica/node-build:latest
 docker build -t node-build:6.9.2 6.9.2
 docker tag node-build:6.9.2 lgatica/node-build:6.9.2
 docker tag node-build:6.9.2 lgatica/node-build:6.9
@@ -12,11 +12,11 @@ docker build -t node-build:4.7.0 4.7.0
 docker tag node-build:4.7.0 lgatica/node-build:4.7.0
 docker tag node-build:4.7.0 lgatica/node-build:4.7
 docker tag node-build:4.7.0 lgatica/node-build:4
-docker build -t node-build:7.3.0-onbuild 7.3.0/onbuild
-docker tag node-build:7.3.0-onbuild lgatica/node-build:7.3.0-onbuild
-docker tag node-build:7.3.0-onbuild lgatica/node-build:7.3-onbuild
-docker tag node-build:7.3.0-onbuild lgatica/node-build:7-onbuild
-docker tag node-build:7.3.0-onbuild lgatica/node-build:onbuild
+docker build -t node-build:7.4.0-onbuild 7.4.0/onbuild
+docker tag node-build:7.4.0-onbuild lgatica/node-build:7.4.0-onbuild
+docker tag node-build:7.4.0-onbuild lgatica/node-build:7.4-onbuild
+docker tag node-build:7.4.0-onbuild lgatica/node-build:7-onbuild
+docker tag node-build:7.4.0-onbuild lgatica/node-build:onbuild
 docker build -t node-build:6.9.2-onbuild 6.9.2/onbuild
 docker tag node-build:6.9.2-onbuild lgatica/node-build:6.9.2-onbuild
 docker tag node-build:6.9.2-onbuild lgatica/node-build:6.9-onbuild
