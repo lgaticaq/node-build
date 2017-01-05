eca044eda8ce157d6717d90583310b38a32fd5e6
Leonardo Gatica <lgatica@protonmail.com>
2017-01-05 15:40:34 +0000

    feat(node): Update node from 4.7.0 to 4.7.1

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ docker build -t node-build:6.9.3 6.9.3
 docker tag node-build:6.9.3 lgatica/node-build:6.9.3
 docker tag node-build:6.9.3 lgatica/node-build:6.9
 docker tag node-build:6.9.3 lgatica/node-build:6
-docker build -t node-build:4.7.0 4.7.0
-docker tag node-build:4.7.0 lgatica/node-build:4.7.0
-docker tag node-build:4.7.0 lgatica/node-build:4.7
-docker tag node-build:4.7.0 lgatica/node-build:4
+docker build -t node-build:4.7.1 4.7.1
+docker tag node-build:4.7.1 lgatica/node-build:4.7.1
+docker tag node-build:4.7.1 lgatica/node-build:4.7
+docker tag node-build:4.7.1 lgatica/node-build:4
 docker build -t node-build:7.4.0-onbuild 7.4.0/onbuild
 docker tag node-build:7.4.0-onbuild lgatica/node-build:7.4.0-onbuild
 docker tag node-build:7.4.0-onbuild lgatica/node-build:7.4-onbuild
@@ -21,8 +21,8 @@ docker build -t node-build:6.9.3-onbuild 6.9.3/onbuild
 docker tag node-build:6.9.3-onbuild lgatica/node-build:6.9.3-onbuild
 docker tag node-build:6.9.3-onbuild lgatica/node-build:6.9-onbuild
 docker tag node-build:6.9.3-onbuild lgatica/node-build:6-onbuild
-docker build -t node-build:4.7.0-onbuild 4.7.0/onbuild
-docker tag node-build:4.7.0-onbuild lgatica/node-build:4.7.0-onbuild
-docker tag node-build:4.7.0-onbuild lgatica/node-build:4.7-onbuild
-docker tag node-build:4.7.0-onbuild lgatica/node-build:4-onbuild
+docker build -t node-build:4.7.1-onbuild 4.7.1/onbuild
+docker tag node-build:4.7.1-onbuild lgatica/node-build:4.7.1-onbuild
+docker tag node-build:4.7.1-onbuild lgatica/node-build:4.7-onbuild
+docker tag node-build:4.7.1-onbuild lgatica/node-build:4-onbuild
 docker push lgatica/node-build
